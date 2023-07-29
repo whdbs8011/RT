88a6d8ab3bec7673fcd0fcf34883d3f58f0b8bc9
whdbs <138410511+whdbs8011@users.noreply.github.com>
2023-07-30 00:32:29 +0900

    로또번호 생성.py

---
+++ 로또번호 생성.py
@@ -0,0 +1,17 @@
+from random import *
+
+print (int(random() * 10))
+print (int(random() * 10) + 1)
+print (int(random() * 10) + 2)
+
+
+print(int(random() * 45) + 1)
+print(int(random() * 45) + 1)
+print(int(random() * 45) + 1)
+print(int(random() * 45) + 1)
+print(int(random() * 45) + 1)
+print(int(random() * 45) + 1)
+
+print (randint(3, 28))
+
+print (randint(1 , 2))
